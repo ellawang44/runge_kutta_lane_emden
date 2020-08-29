3725a719f006c8765f11d959623301cb6ce83c2d
ella wang <ellawang@ellas-MacBook-Pro.local>
2020-08-29 10:52:19 +1000

    Non integer n values now work. Cleaned up main with function

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import runge_kutta
 import argparse
-# typing LaTeX code in matplotlib graphs
-from matplotlib import rc
-rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-## for Palatino and other serif fonts use:
-#rc('font',**{'family':'serif','serif':['Palatino']})
-rc('text', usetex=True)
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
 
 # parsers so you don't need to edit the code to test it
 parser = argparse.ArgumentParser(description='runge-kutta integration of the Lane-Emden equation.')
@@ -19,15 +11,11 @@ parser.add_argument('-t', '--tstep', metavar = 'time_step', type = float, defaul
 parser.add_argument('--check', action = 'store_true', default = False, help='Change if analytical plot is shown or not for n = 0 and n = 1, default is off.')
 parser.add_argument('--eval', action = 'store_true', default = False, help='Change if xi_1 and d theta/d xi (xi_1) is printed or not, default is off.')
 
-if __name__ == '__main__':
-    args = parser.parse_args()
-    # define integration variables
-    n = args.nthorder
-    t_step = args.tstep
-
+def rk_integrate(n, t_step):
+    '''Apply runge-kutta integration to the lane-emden equation'''
     # define initial conditions
     x_0 = 1e-50 # need some small value that is close to 0 but not exactly 0
-    y_0 = 1
+    y_0 = 1.0
     z_0 = 0
 
     # do the integration and compile the results into two lists
@@ -35,8 +23,19 @@ if __name__ == '__main__':
     ys = [y_0]
     while y_0>0:
         x_0, y_0, z_0 = runge_kutta.integrate(x_0, y_0, z_0, n, t_step)
+        if type(y_0) == complex: # complex values of y_0 sometimes occur when n is not an integer
+            break
         xs.append(x_0)
         ys.append(y_0)
+    return (xs, ys)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    # define integration variables
+    n = args.nthorder
+    t_step = args.tstep
+
+    xis, thetas = rk_integrate(n, t_step)
 
     # checking that the runge-kutta method is valid
     if args.check:
@@ -52,9 +51,10 @@ if __name__ == '__main__':
         print('d theta / d xi (xi_1) =', z_0)
 
     # plots
-    plt.plot(xs, ys, label = 'runge-kutta')
+    plt.ion()
+    plt.plot(xis, thetas, label = 'runge-kutta')
     if (args.check and n==0) or (args.check and n==1):
-        plt.plot(xs, np.vectorize(check)(xs), label = 'actual')
+        plt.plot(xis, np.vectorize(check)(xis), label = 'actual')
         plt.legend()
     plt.xlabel(r'$\xi$')
     plt.ylabel(r'$\theta$')
